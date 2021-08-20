15bc806f6b460a45c2e3638140cb1cf697117195
UmmeNafisa <pappanafisa@gmail.comm>
2021-08-21 01:29:32 +0600

    memory-handler-add

--- js/macBook.js
+++ js/macBook.js
@@ -1,1 +1,16 @@
-console.log("here i am")
+// memory part handler
+function extraMemoryCost(typeGB) {
+    const memoryCost = document.getElementById("memory-cost");
+    const memoryCostValue = memoryCost.innerText;
+    if (typeGB == "16GB") {
+        memoryCost.innerText = 180;
+    } else {
+        memoryCost.innerText = 0;
+    }
+}
+document.getElementById("memory1-btn").addEventListener("click", function () {
+    extraMemoryCost("8GB");
+})
+document.getElementById("memory2-btn").addEventListener("click", function () {
+    extraMemoryCost("16GB");
+})
